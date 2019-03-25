611a539fa762fd1a8294b719a7eb92c5293c4dcc
tylerwooten <tylerwooten@tamu.edu>
2019-03-25 14:16:19 -0500

    Commented Kar Tech Encoder function

--- Kar_tech_encoder.py
+++ Kar_tech_encoder.py
@@ -1,20 +1,33 @@
 ##### Howdy ####
 
-def Kar_tech_encoder(identifier, position):
+def Kar_tech_encoder(position):
+    # identifier is no longer needed. This is defined in the send bus message.
+    # position in inches that the actuator should go to
+
+    # initializing 8 byte container
     can_message = ['00', '00', '00', '00', '00', '00', '00', '00']
+    # byte 0 and byte 1 will always be these values
     can_message[0] = '0f'
     can_message[1] = '4a'
 
+    # position (inches) multiplied by 1000
     adjusted_position = position * 1000
+    # offset defined by Kar Tech Actuator documentation
     offset = 500
+    # Value that needs to be turned into hexadecimal
     adjusted_position_with_offset = adjusted_position + offset
 
+    # Changing decimal to hex value
     hex_value = hex(int(adjusted_position_with_offset))
+    # Removing '0x' from the start of the hex value
     hex_value_no_0x = hex_value[-3:]
 
+    # Take last two of hex value and set to byte 2
     byte_2_hex = hex_value_no_0x[-2:]
+    # Take first letter of hex value and set to byte 3
     byte_3_hex = 'c' + hex_value_no_0x[0]
 
+    # Assigning bytes to the can message
     can_message[2] = byte_2_hex
     can_message[3] = byte_3_hex
 
@@ -23,14 +36,15 @@ def Kar_tech_encoder(identifier, position):
     print('position: ', position)
     print('can message: ', can_message)
 
+    # Initializing empty container
     send_message = []
-
+    # Looping through bytes in can message and turning into decimal
     for item in can_message:
         item = int(item, 16)
         send_message.append((item))
 
 
     print('send message: ', send_message)
-    return send_message
+    return send_message  # list of integers format -> [00, 00, 00, 00, 00, 00, 00, 00]
 
-Kar_tech_encoder('0x00FF0000', 2)
+Kar_tech_encoder(2)
--- Steering_With_Xbox.py
+++ Steering_With_Xbox.py
@@ -66,37 +66,47 @@ def command_encoder(position, speed):
     return message_send
 
 
-def Kar_tech_encoder(identifier, position):
+def Kar_tech_encoder(position):
+    # identifier is no longer needed. This is defined in the send bus message.
+    # position in inches that the actuator should go to
+
+    # initializing 8 byte container
     can_message = ['00', '00', '00', '00', '00', '00', '00', '00']
+    # byte 0 and byte 1 will always be these values
     can_message[0] = '0f'
     can_message[1] = '4a'
 
+    # position (inches) multiplied by 1000
     adjusted_position = position * 1000
+    # offset defined by Kar Tech Actuator documentation
     offset = 500
+    # Value that needs to be turned into hexadecimal
     adjusted_position_with_offset = adjusted_position + offset
 
+    # Changing decimal to hex value
     hex_value = hex(int(adjusted_position_with_offset))
+    # Removing '0x' from the start of the hex value
     hex_value_no_0x = hex_value[-3:]
 
+    # Take last two of hex value and set to byte 2
     byte_2_hex = hex_value_no_0x[-2:]
+    # Take first letter of hex value and set to byte 3
     byte_3_hex = 'c' + hex_value_no_0x[0]
 
+    # Assigning bytes to the can message
     can_message[2] = byte_2_hex
     can_message[3] = byte_3_hex
-
 
     print('identifier: ', identifier)
     print('position: ', position)
     print('can message: ', can_message)
 
-    send_message = []
-
+    # Initializing empty container
+    send_message = [] # list of integers format -> [00, 00, 00, 00, 00, 00, 00, 00]
+    # Looping through bytes in can message and turning into decimal
     for item in can_message:
         item = int(item, 16)
         send_message.append((item))
-
-
-    print('send message: ', send_message)
 
     message_send_actuator = Message(arbitration_id=16711680, data=send_message)
     return message_send_actuator
